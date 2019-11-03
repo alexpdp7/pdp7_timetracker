b1604f61d09fe05ab51d48a956e69cdb49097722
alex <alex@pdp7.net>
2019-11-03 12:20:22 +0100

    Move stuff from test_base to conftest
    This also silences some warnings

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,49 +1,6 @@
 import collections
 import datetime
 
-import pytest
-from testcontainers import postgres
-
-import pdp7_timetracker
-from pdp7_timetracker import sql
-
-
-class TestSql(sql.Sql):
-    def create_result(self, cursor):
-        return cursor.fetchall()
-
-
-class TestTimeTracker(sql.SqlTimeTracker):
-    def __init__(self, time_tracker, database):
-        super().__init__(time_tracker, TestSql)
-        self.connection = sql.connect(
-            database.get_connection_url().replace("+psycopg2", "")
-        )
-
-    def _open_connection(self):
-        return self.connection
-
-    def _close_connection(self, connection):
-        pass
-
-
-@pytest.fixture(scope="session")
-def database():
-    with postgres.PostgresContainer("postgres:12") as pg:
-        with sql.connect(
-            pg.get_connection_url().replace("+psycopg2", "")
-        ) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(pdp7_timetracker.TimeTracker().get_schema())
-        yield pg
-
-
-@pytest.fixture
-def tt(database):
-    tt = TestTimeTracker(pdp7_timetracker.TimeTracker(), database)
-    yield tt
-    tt.connection.rollback()
-
 
 def test_new_activity(tt):
     tt.new_activity("foo")
